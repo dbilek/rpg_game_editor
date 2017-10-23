0756da345076f1e7617479b4ebe2a70d1d6fec71
Danjo <danjobilek@gmail.com>
2017-10-23 11:07:57 +0200

    Styling character attributes

---
+++ spec/factories/character_attributes.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :character_attribute do
+  	id 1
+    username "example_name"
+    intensity 50
+    icon "default_icon.png"
+    character_id ""
+  end
+end
--- spec/request/character_attributes/create_character_attributes_spec.rb
+++ spec/request/character_attributes/create_character_attributes_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-describe "Create character attributes" do
+describe "Create character attributes", :js => true do
 	let(:user) { FactoryGirl.create(:user) }
 	let(:character) { FactoryGirl.create(:character, user_id: user.id) }
+	let(:character_attribute) { FactoryGirl.create(:character_attribute, character_id: character.id) }
 
   it "should create character attributes" do
     sign_in(user)
@@ -10,9 +11,10 @@ describe "Create character attributes" do
     visit character_path(character.id)
     fill_in "Name", :with => "Power"
     fill_in "Intensity", :with => 60
-    attach_file("Upload icon", Rails.root + "spec/fixtures/default_icon.png")
+    # attach_file("Upload icon", Rails.root + "spec/fixtures/default_icon.png")
     click_button('Add attribute')
-    expect(page).to have_content("Attributes")
+    expect(page).to have_selector :css, '.attribute_1'
+
   end
 
 end
